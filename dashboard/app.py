5fef71603439687bc0b5eeb1b51e522cf9282744
rawanessaam <https://github.com/rawanessaam/UI-for-Vision-Feature-Selection-Neural-Evolution-Platform-cloud-project>
2026-05-08 17:58:46 +0300

    commit

--- dashboard/app.py
+++ dashboard/app.py
@@ -28,7 +28,7 @@ from utils import (
 )
 from charts import (
     plot_accuracy_comparison, plot_ga_convergence,
-    plot_runtime_comparison, plot_confidence_distribution,
+    plot_confidence_distribution,
     plot_experiment_metrics_overview, plot_feature_selection,
 )
 
